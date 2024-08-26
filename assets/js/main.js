ec1c47f168d65116e6105f1dc29483f5c1efab00
Ian Lionetti <ilionetti@uade.edu.ar>
2024-08-25 23:13:11 -0300

    nuevo commit2

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,7 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+
 const sections = document.querySelectorAll("section[id]");
 
 window.addEventListener("scroll", scrollActive);
